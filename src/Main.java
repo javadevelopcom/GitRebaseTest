64488548ff072a1eb7c5120d1468a0820d124ad7
Hardcore <java.develop.com@gmail.com>
2015-11-16 00:09:22 +0200

    waiting for comment3

--- src/Main.java
+++ src/Main.java
@@ -6,4 +6,5 @@ public class Main {
 // http://habrahabr.ru/post/161009/
 // https://git-scm.com/book/ru/v1/%D0%92%D0%B5%D1%82%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5-%D0%B2-Git-%D0%A3%D0%B4%D0%B0%D0%BB%D1%91%D0%BD%D0%BD%D1%8B%D0%B5-%D0%B2%D0%B5%D1%82%D0%BA%D0%B8
 
-//comment 3 for fetch
+
+// no info
